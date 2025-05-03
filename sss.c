99d2d0729df1ca53847f804ae9f8af7f0b457fd9
KatsuraHina <140583649+KatsuraHina@users.noreply.github.com>
2025-05-03 19:50:11 +0800

    Add files via upload
    Implemented the multi-threaded sorting with main thread blocking and signallingg

--- sss.c
+++ sss.c
@@ -1,40 +1,38 @@
-#include <stdio.h>              // For printf, fopen, fscanf, etc.
-#include <stdlib.h>             // For malloc, free, exit
-#include <pthread.h>            // For pthread functions
-#include <unistd.h>             // For usleep (optional delay)
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-#define MAX_SIZE 200            // Maximum number of integers allowed in input
+#define MAX_SIZE 200
 
-// Create aliases to make types shorter and cleaner
 typedef pthread_mutex_t PMutex;
 typedef pthread_cond_t PCond;
 
-// Global array to store integers from file
 int A[MAX_SIZE];
-int n;                          // Number of integers read from the file
-int total_swaps = 0;           // Total number of swaps done by both threads
-int swap_t1 = 0;               // Number of swaps done by thread T1
-int swap_t2 = 0;               // Number of swaps done by thread T2
+int n;
+int total_swaps = 0;
+int swap_t1 = 0;
+int swap_t2 = 0;
 
-// Thread synchronization
-PMutex lock = PTHREAD_MUTEX_INITIALIZER;   // Mutex to protect shared resources
-PCond cond_t1 = PTHREAD_COND_INITIALIZER;  // Condition variable for T1
-PCond cond_t2 = PTHREAD_COND_INITIALIZER;  // Condition variable for T2
+PMutex lock = PTHREAD_MUTEX_INITIALIZER;
+PCond cond_t1 = PTHREAD_COND_INITIALIZER;
+PCond cond_t2 = PTHREAD_COND_INITIALIZER;
+PMutex main_mutex = PTHREAD_MUTEX_INITIALIZER;
+PCond cond_main = PTHREAD_COND_INITIALIZER;
 
-int turn = 1;                  // Whose turn it is: 1 = T1, 2 = T2
-int stop = 0;                  // Flag to signal both threads to terminate
+int turn = 1;
+int stop = 0;
 
-// The sorting function run by both threads
 void* sort(void* arg) {
-    int id = *(int*)arg;       // Get thread ID (1 for T1, 2 for T2)
-    free(arg);                 // Free dynamically allocated ID
+    int id = *(int*)arg;
+    free(arg);
 
     printf("Thread T%d starting\n", id);
 
     while (1) {
-        pthread_mutex_lock(&lock);  // Lock the mutex to enter critical section
+        pthread_mutex_lock(&lock);
 
-        if (stop) {                 // If the stop flag is set, exit loop
+        if (stop) {
             pthread_mutex_unlock(&lock);
             break;
         }
@@ -47,7 +45,7 @@ void* sort(void* arg) {
                 pthread_cond_wait(&cond_t2, &lock);
         }
 
-        int local_swap = 0;   // Track number of swaps in this round
+        int local_swap = 0;
 
         if (id == 1) {
             // Thread T1 compares A[2i] and A[2i+1]
@@ -60,11 +58,10 @@ void* sort(void* arg) {
                     local_swap++;
                 }
             }
-            swap_t1 = local_swap;       // Save T1's swap count
-            total_swaps += local_swap;  // Update total swap count
-            turn = 2;                   // Now it's T2's turn
-            pthread_cond_signal(&cond_t2); // Wake up T2
-
+            swap_t1 = local_swap;
+            total_swaps += local_swap;
+            turn = 2;
+            pthread_cond_signal(&cond_t2);  // Signal T2
         } else {
             // Thread T2 compares A[2i-1] and A[2i]
             for (int i = 1; i <= (n - 1) / 2; i++) {
@@ -76,45 +73,46 @@ void* sort(void* arg) {
                     local_swap++;
                 }
             }
-            swap_t2 = local_swap;       // Save T2's swap count
-            total_swaps += local_swap;  // Update total swap count
+            swap_t2 = local_swap;
+            total_swaps += local_swap;
 
-            // If both threads made 0 swaps, sorting is done
             if (swap_t1 == 0 && swap_t2 == 0) {
-                stop = 1;                   // Set flag to terminate
-                pthread_cond_signal(&cond_t1); // Wake up T1 if waiting
-                pthread_cond_signal(&cond_t2); // Wake up T2 if waiting
+                stop = 1;
+                pthread_cond_signal(&cond_t1);
+                pthread_cond_signal(&cond_t2);
             } else {
-                turn = 1;               // Next round is T1's turn
-                pthread_cond_signal(&cond_t1); // Wake up T1
+                turn = 1;
+                pthread_cond_signal(&cond_t1);  // Signal T1
             }
         }
 
-        pthread_mutex_unlock(&lock);  // Exit critical section
+        pthread_mutex_unlock(&lock);
 
-        usleep(100000);               // Optional delay for visibility (100ms)
+         // Signal the main thread after each iteration
+        pthread_mutex_lock(&main_mutex);
+        pthread_cond_signal(&cond_main);
+        pthread_mutex_unlock(&main_mutex);
+
+
+       usleep(100000);
     }
 
-    // Print result for this thread
     printf("Thread ID%d: total number of swaps = %d\n", id, (id == 1 ? swap_t1 : swap_t2));
-    pthread_exit(NULL);              // Exit thread
+    pthread_exit(NULL);
 }
 
 int main(int argc, char* argv[]) {
-    // Check if input file is provided
     if (argc != 2) {
         printf("Usage: %s ToSort\n", argv[0]);
         return 1;
     }
 
-    // Open the input file for reading
     FILE* file = fopen(argv[1], "r");
     if (!file) {
         perror("File open failed");
         return 1;
     }
 
-    // Read integers from file into array A
     n = 0;
     while (fscanf(file, "%d", &A[n]) == 1 && n < MAX_SIZE) {
         n++;
@@ -123,7 +121,6 @@ int main(int argc, char* argv[]) {
 
     printf("Main thread: read %d integers\n", n);
 
-    // Create thread IDs dynamically
     pthread_t t1, t2;
     int* id1 = malloc(sizeof(int));
     int* id2 = malloc(sizeof(int));
@@ -132,20 +129,27 @@ int main(int argc, char* argv[]) {
 
     printf("Main thread: starting threads...\n");
 
-    // Create the two sorting threads
     pthread_create(&t1, NULL, sort, id1);
     pthread_create(&t2, NULL, sort, id2);
 
-    // Wait for both threads to complete
+    // Main thread loop to wait and check 'stop'
+    pthread_mutex_lock(&main_mutex);
+    while (!stop) {
+        pthread_cond_wait(&cond_main, &main_mutex);
+    }
+    pthread_mutex_unlock(&main_mutex);
+
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
-    // Print final sorted array
     printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", A[i]);
     }
     printf("\nTotal number of swaps to sort array A = %d\n", total_swaps);
 
-    return 0;   // Program completed successfully
+    free(id1);
+    free(id2);
+
+    return 0;
 }
